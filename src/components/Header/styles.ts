75c343e46f8a5a1eda04eedbf51f22917f09b908
William Moreschi <williammoreschi@gmail.com>
2021-02-04 22:12:03 -0300

    feat: Adicionando o component Header e seus estilos

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2265ff',
+    marginBottom: 20,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textHeader: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 22,
+    color: '#fff',
+  },
+});
